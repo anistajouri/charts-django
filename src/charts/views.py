a1cc700dd5d01be30a6d27b7ef79b50e05bd37bf
anis.tajouri <anis.tajouri@gmail.com>
2017-11-09 12:27:35 +0100

    update for demo

--- src/charts/views.py
+++ src/charts/views.py
@@ -5,22 +5,19 @@ from django.views.generic import View
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
 
 
 User = get_user_model()
 
-class HomeView(View):
+class charts1(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'charts.html', {"customers": 10})
+        return render(request, 'charts.html', {})
 
 
-
-def get_data(request, *args, **kwargs):
-    data = {
-        "sales": 100,
-        "customers": 10,
-    }
-    return JsonResponse(data) # http response
+class charts2(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'charts2.html', {})
 
 
 class ChartData(APIView):
@@ -28,9 +25,22 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        qs_count = User.objects.all().count()
-        labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
-        default_items = [qs_count, 23, 2, 3, 12, 2]
+        with open("aggr1.txt", 'rt') as f:
+            aggr1 = json.loads(f.read().replace('\n', ''))
+        buckets=aggr1["aggregations"]["all_counties"]["buckets"]
+        print(aggr1["aggregations"]["all_counties"]["buckets"][0])
+        #qs_count = User.objects.all().count()
+        labels =  []
+        default_items = []
+        for count in range(0,len(buckets)):
+            print(buckets[count]["key"])
+            labels.append(buckets[count]["key"]) 
+            default_items.append(int(buckets[count]["doc_count"]))
+        print(labels)
+        print(default_items)
+
+        # labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        # default_items = [qs_count, 23, 2, 3, 12, 2]
         data = {
                 "labels": labels,
                 "default": default_items,
